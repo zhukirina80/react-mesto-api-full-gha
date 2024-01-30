ba24584d3d21c4fda828df40cf536c074234ebb2
Irina Zhukovskaya <zhukirina80@mail.ru>
2024-01-30 16:02:16 +0300

    Меняет адрес подключения к базе данных

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(errors());
 
 app.use(serverError);
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
